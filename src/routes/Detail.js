4e417fae70b9337376ab9af6ff9b42d9e6e3f292
Kassia <coffeezaroo@gmail.com>
2023-06-03 22:44:32 +0900

    Detail 수정 : css 적용

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Detail.css';
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -13,7 +14,21 @@ class Detail extends React.Component {
     const { location } = this.props;
 
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      return (
+        <div className="detail_container">
+          <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+          <div className="detail__data">
+            <h3 className="detail__title">{location.state.title}</h3>
+            <h5 className="detail__year">{location.state.year}</h5>
+            <ul className="detail__genres">
+              {location.state.genres.map((genre, index) => {
+                return <li key={index} className="detail__genre">{genre}</li>;
+              })}
+            </ul>
+            <p className="detail__summary">{location.state.summary}</p>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
