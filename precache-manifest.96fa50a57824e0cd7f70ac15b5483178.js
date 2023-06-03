4d4da212a9fe5eb8b42f5c8aeffebea3c5d90db2
Kassia <coffeezaroo@gmail.com>
2023-06-03 22:25:31 +0900

    Updates

---
+++ precache-manifest.96fa50a57824e0cd7f70ac15b5483178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00571fd93e4fc20d11eef63a8e81db9f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f15131b550970f3b15ad",
+    "url": "/movie_app_2020/static/css/main.e31ab0be.chunk.css"
+  },
+  {
+    "revision": "d5971d346ca8ae802af8",
+    "url": "/movie_app_2020/static/js/2.7c88bfcc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_2020/static/js/2.7c88bfcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15131b550970f3b15ad",
+    "url": "/movie_app_2020/static/js/main.6eded150.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/movie_app_2020/static/js/main.6eded150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5912edef788f053f9868",
+    "url": "/movie_app_2020/static/js/runtime-main.d6f248e5.js"
+  }
+]);
