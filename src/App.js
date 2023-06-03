dbc9ab4b64624c9f7c2a16301bf531bc79b969b8
Kassia <coffeezaroo@gmail.com>
2023-06-03 14:35:06 +0900

    [클론 코딩 05] state와 클래스형 컴포넌트 Ⅰ
    (1) state로 숫자 증감 기능 만들어 보기
    (2) 숫자 증감 기능을 제대로 만들어 보기
    (3) 클래스형 컴포넌트의 일생 알아보기

--- src/App.js
+++ src/App.js
@@ -1,61 +1,51 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgZnNhJ-WoAjK6ejfdB90QNxPo7XsdRTk_UA&usqp=CAU',
-    rating: 5
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmOdNf-Fp-tOAy_QVJce8D99UxfaPQzkyA1w&usqp=CAU',
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-gz-SdYi0qkBq3MbLbI4M9EVxZSAN6UB2tw&usqp=CAU',
-    rating: 3.7
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSumtIvCsMZnl7WGHc2jLEYNaT43QhCdk1fUtdtiPs__ie5bg5ThfTvX0znMFEe8WQWSEE&usqp=CAU',
-    rating: 2
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs5uFUs2rCl_GCg-nudLZjPykm2s1veKBxGQ&usqp=CAU',
-    rating: 4.2
+    console.log('hello');
   }
-];
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />))}
-    </div>
-  );
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1
+    }));
+  };
+
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1
+    }));
+  };
+
+  componentDidMount() {
+    console.log('component rendered');
+  }
+
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world');
+  }
+
+  render() {
+    console.log("I'm rendering");
+
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
 
 export default App;
